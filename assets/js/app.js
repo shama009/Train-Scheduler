904a27fee072000335d5c19698b42104a09ff7bb
Shama <shamaanjum09@outlook.com>
2017-12-09 16:41:44 -0800

    basic page layout index.html

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// initialize variables
+// initialize firebase database
+// get user input form data
+// store user input in firebase
+// as user enters new data get the data from firebase
+// caliculate next arrival and minutes away values
+// display data to html page.
+
+$(document).ready(function(){
+    var trainName = $("#train-name-input").val().trim();
+});
